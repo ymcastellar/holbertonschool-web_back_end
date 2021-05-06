8aaafdbdc5d31567a59bb44da1cc592060858d0b
ymcastellar <yoycas@hotmail.com>
2021-05-05 19:34:14 -0500

    fixing pycodestyle

--- 0x04-pagination/0-simple_helper_function.py
+++ 0x04-pagination/0-simple_helper_function.py
@@ -7,5 +7,5 @@ from typing import Tuple
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """return a tuple of size two containing a start index and an end index"""
-    
+
     return (page * page_size - page_size, page * page_size)
