6e8c7943c27ed615f244b1f2e6772a7b8977c350
ymcastellar <yoycas@hotmail.com>
2021-05-19 15:42:52 -0500

    User ID for Session ID added

--- 0x07-Session_authentication/api/v1/auth/session_auth.py
+++ 0x07-Session_authentication/api/v1/auth/session_auth.py
@@ -16,3 +16,9 @@ class SessionAuth (Auth):
             session_id = str(uuid.uuid4())
             SessionAuth.user_id_by_session_id[session_id] = user_id
             return session_id
+    
+    
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """ session id """
+        if isinstance(session_id, str):
+            return SessionAuth.user_id_by_session_id.get(session_id)
