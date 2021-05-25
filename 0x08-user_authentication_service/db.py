b15ab9c823cf79e256f7db6857921ce7dc621176
ymcastellar <yoycas@hotmail.com>
2021-05-25 14:30:06 -0500

    fixing class

--- 0x08-user_authentication_service/db.py
+++ 0x08-user_authentication_service/db.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
-"""DB module
+"""
+DB module
 """
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
@@ -9,29 +10,33 @@ from user import Base, User
 
 
 class DB:
-    """DB class
     """
-
-    def __init__(self) -> None:
-        """Initialize a new DB instance
+    DB class
+    """
+    
+    def __init__(self):
         """
-        self._engine = create_engine("sqlite:///a.db", echo=True)
+        Initialize DB
+        """
+        self._engine = create_engine("sqlite:///a.db", echo=False)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
 
     @property
-    def _session(self) -> Session:
-        """Memoized session object
+    def _session(self):
+        """
+        Session
         """
         if self.__session is None:
             DBSession = sessionmaker(bind=self._engine)
             self.__session = DBSession()
         return self.__session
 
-
     def add_user(self, email: str, hashed_password: str) -> User:
-        """add new user to db"""
+        """
+        Save a user to the database
+        """
         user = User(email=email, hashed_password=hashed_password)
         self._session.add(user)
         self._session.commit()
