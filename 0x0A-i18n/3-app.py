e64e7cadab0ed0a4c0f25565046cef5fd9bb7dc2
ymcastellar <yoycas@hotmail.com>
2021-06-03 19:49:48 -0500

    Parametrize templates added

---
+++ 0x0A-i18n/3-app.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+"""
+Flask app
+"""
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+class Config:
+    """
+    Config class
+    """
+    LANGUAGES = ['en', 'fr']
+
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+app.config.from_object(Config)
+
+babel = Babel(app)
+Babel.default_locale = 'en'
+Babel.default_timezone = 'UTC'
+
+
+@app.route('/', methods=['GET'])
+def hello():
+    """ return index """
+    return render_template('3-index.html')
+
+
+@babel.localeselector
+def get_locale():
+    """
+    Get locale from request
+    """
+    return request.accept_languages.best_match(Config.LANGUAGES)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
