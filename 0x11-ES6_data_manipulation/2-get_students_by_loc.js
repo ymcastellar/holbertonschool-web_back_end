5b4d55212167031cbad118739cb46f850b8c681e
ymcastellar <yoycas@hotmail.com>
2021-06-29 16:37:38 -0500

    adding semicolon

--- 0x11-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x11-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,3 +1,3 @@
 export default function getStudentsByLocation(listOfStudents, city) {
-  return listOfStudents.filter((student) => student.location === city)
+  return listOfStudents.filter((student) => student.location === city);
 }
