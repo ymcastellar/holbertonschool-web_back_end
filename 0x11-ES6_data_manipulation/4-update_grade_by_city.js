c293dc9aee31182a754d5d951151add79cedfaae
ymcastellar <yoycas@hotmail.com>
2021-06-29 16:56:15 -0500

    Combine

---
+++ 0x11-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,8 @@
+export default function updateStudentGradeByCity(listStudents, city, newGrades) {
+  return listStudents.filter(
+    (student) => student.location === city,
+  ).map((student) => Object.assign(
+    student,
+    { grade: newGrades.filter((grade) => student.id === grade.studentId)[0] ? newGrades.filter((grade) => student.id === grade.studentId)[0].grade : 'N/A' },
+  ));
+}
