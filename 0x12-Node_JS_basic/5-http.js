fdb94f8c5015e5092163b504a3a3f4088273dac9
ymcastellar <yoycas@hotmail.com>
2021-07-05 16:32:46 -0500

    create http

--- 0x12-Node_JS_basic/5-http.js
+++ 0x12-Node_JS_basic/5-http.js
@@ -1,29 +1,23 @@
 const http = require('http');
 const countStudents = require('./3-read_file_async');
 
-const port = 1245;
-
-const app = http.createServer((req, res) => {
-  const { method, url } = req;
-  if (method === 'GET' && url === '/') {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello Holberton School!');
-  }
-  if (method === 'GET' && url === '/students') {
-    countStudents(String(process.argv.slice(2)))
-      .then((arrayOfClasses) => {
+const app = http
+  .createServer((req, res) => {
+    console.log(process.argv);
+    switch (req.url) {
+      case '/':
+        res.end('Hello Holberton School!');
+        break;
+      case '/students':
         res.write('This is the list of our students\n');
-        res.write(`Number of students: ${arrayOfClasses.count}\n`);
-        for (const cls in arrayOfClasses) {
-          if (cls && cls !== 'count') res.write(`Number of students in ${cls}: ${arrayOfClasses[cls].length}. List: ${arrayOfClasses[cls].join(', ')}\n`);
-        }
-        res.end();
-      })
-      .catch((err) => { throw err; });
-  }
-});
-
-app.listen(port);
+        countStudents(process.argv[2])
+          .then((data) => res.end(data))
+          .catch((err) => res.end(err.message));
+        break;
+      default:
+        break;
+    }
+  })
+  .listen(1245);
 
 module.exports = app;
