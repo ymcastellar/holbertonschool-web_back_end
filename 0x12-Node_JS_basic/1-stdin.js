71447c32d3c766410da680236215b542876fd077
ymcastellar <yoycas@hotmail.com>
2021-07-05 15:50:40 -0500

    fixing eslint style

--- 0x12-Node_JS_basic/1-stdin.js
+++ 0x12-Node_JS_basic/1-stdin.js
@@ -1,12 +1,7 @@
-import { createInterface } from 'readline';
-
-const rl = createInterface({
-  input: process.stdin,
-  output: process.stdout,
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+process.stdin.on('readable', () => {
+  const name = process.stdin.read();
+  if (name) console.log(`Your name is: ${name}`);
 });
 
-rl.question('Welcome to Holberton School, what is your name?\n', (answer) => {
-  console.log(`Your name is: ${answer}`);
-  console.log('This important software is now closing');
-  process.exit();
-});
+if (process.stdout.isTTY) process.stdin.on('end', () => process.stdout.write('This important software is now closing\n'));
